911b8b50e43084df24a68e1c74e8a1223abe72f8
jhoampi <jprietohein@gmail.com>
2024-05-26 22:34:56 -0400

    Tarea2 Parte 1

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-package com.example.tarea21;
-
 import Interfaz.EscenaMascota;
 import Interfaz.PanelDeEstado;
 import Interfaz.InventarioGC;
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.tarea21 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.tarea21 to javafx.fxml;
-    exports com.example.tarea21;
-}

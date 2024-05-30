eefb9a60f00f5e95fd990482187b5a2f4ab7a6ae
jhoampi <jprietohein@gmail.com>
2024-05-30 19:29:12 -0400

    Todos los archivos al main

--- Tarea2-EtapaFinal/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tarea2etapafinal {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tarea2etapafinal to javafx.fxml;
-    exports com.example.tarea2etapafinal;
-}
